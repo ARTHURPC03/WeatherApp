3666451cdb30984a53c370db9e5a29bd25040c34
Arthur Pimentel Cordeiro <arthurpimetelcordeiro@gmail.com>
2021-07-05 20:32:00 -0300

    ✨style: Hidden status bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 
 import Routes from './routes'
+import { StatusBar } from 'react-native'
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar hidden />
       <Routes />
     </NavigationContainer>
   )
